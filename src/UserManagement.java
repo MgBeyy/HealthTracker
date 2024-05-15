c158f0c3aceb50e1ccff0f094956cdfc81970ea5
Zaid Almoughrabi <zaidalmoughrabi@gmail.com>
2024-05-15 14:21:44 +0300

    Added UserManagement class

---
+++ src/UserManagement.java
@@ -0,0 +1,12 @@
+public class UserManagement {
+    //Kullanıcı oluşturma ve giriş yapma gibi temel işlemleri yapan sınıf
+    //Her yeni kullanıcı bir node olarak linkdlistte tutulacak
+    //Her yeni kullanıcı şifresi bir hashmap olarak saklanack (sadece 8 karekter)
+    //Şifre max 8 karekter ve her karekterin ascii kodunun 8'e moduna göre bir indiste tutulacak.
+    //Çakışma olması halinde linner ilerlenecek
+    //Her giriştte önce username linkedlistte aranacak
+    //username bulunursa girdiği şifreden de hash map oluşturup kyıtlı olanla karşılaştırılacak
+
+    
+
+}
